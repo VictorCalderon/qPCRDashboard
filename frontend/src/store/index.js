9b88e46243232b16b76ddf1d70a2d8ee28aac18d
Victor Calderon <victorvirgilioc@gmail.com>
2020-06-08 04:12:16 +0000

    Project -> Experiment

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -5,18 +5,18 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 // Import Store Modules
-import Projects from '@/store/modules/projects'
+import Experiments from '@/store/modules/experiments'
 import Samples from '@/store/modules/samples'
-import qPCRs from '@/store/modules/qpcrs'
+import Fluorescences from '@/store/modules/fluorescence'
 import Settings from '@/store/modules/settings'
 import Auth from '@/store/modules/auth'
 
 // Export Modules
 export default new Vuex.Store({
   modules: {
-    Projects,
+    Experiments,
     Samples,
-    qPCRs,
+    Fluorescences,
     Settings,
     Auth
   }
