6891ce5935815c23244455926f88003b1b625583
Victor Calderon <victorvirgilioc@gmail.com>
2020-06-08 04:06:56 +0000

    cleaning function imports

--- backend/qpcr_manager/helpers/__init__.py
+++ backend/qpcr_manager/helpers/__init__.py
@@ -1,14 +1,7 @@
 from qpcr_manager.helpers.preprocessing import (
     feed_DA2, feed_7500, analyze_experiment, 
-    amped_timeseries, available_markers, export_results
+    amped_timeseries, available_markers, export_results, 
+    query_fluorescence
 )
 
-#import_qpcr, feed_qpcrs, query_qpcrs, export_results, feed_zip, analyze_project, dashboard_data, available_markers
-
-__all__ = ['feed_DA2', 'feed_7500', 'analyze_experiment', 'amped_timeseries', 'available_markers']
-
-
-"""
-    'parse_qpcrs', 'import_qpcrs', 'feed_qpcrs', 'query_qpcrs',
-    'export_results', 'feed_zip', 'analyze_project', 'dashboard_data', 'available_markers'
-"""
+__all__ = ['feed_DA2', 'feed_7500', 'analyze_experiment', 'amped_timeseries', 'available_markers', 'query_fluorescence']
