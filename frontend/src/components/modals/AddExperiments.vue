d54afd9179f185d3b178b6d15d1195d4e6d06ee3
VictorCalderon <victorvirgilioc@gmail.com>
2020-06-08 21:38:28 -0400

    workin?

--- frontend/src/components/modals/AddExperiments.vue
+++ frontend/src/components/modals/AddExperiments.vue
@@ -1,129 +1,137 @@
 <template>
   <div>
-    <b-modal
-      id="add-experiments-modal"
-      ref="add-experiments-modal"
-      size="md"
-      hide-footer
-      button-size="md"
-      title="Add a new experiment"
-    >
-      <b-row>
-        <b-col>
-          <b-alert v-model="showAlert" variant="info" class="text-center">{{ importMsg }}</b-alert>
-        </b-col>
-      </b-row>
-      <b-row>
-        <b-col>
-          <b-form-group
-            id="fieldset-1"
-            :description="invalidExperimentName"
-            label="Enter your experiment's name"
-            label-for="input-experimentname"
-            class="text-center"
-          >
-            <b-form-input
-              id="input-experimentname"
-              v-model="name"
-              :state="state"
-              trim
-              placeholder="SARS-CoV-2 Run 1"
-              class="text-center"
-            ></b-form-input>
-            <b-tooltip
-              target="input-experimentname"
-              triggers="hover"
-              placement="right"
-              variant="info"
-            >{{ nameDescription }}</b-tooltip>
-          </b-form-group>
-        </b-col>
-      </b-row>
-      <b-row>
-        <b-col>
-          <b-form-group
-            id="fieldset-1"
-            description="Brief methodology description of your experiment."
-            label="Enter your experiment's methodology"
-            label-for="input-experientmethod"
-            class="text-center"
-          >
-            <b-form-input
-              id="input-experientmethod"
-              v-model="method"
-              :state="state"
-              trim
-              placeholder="extraction-primerprobe-cycling"
-              class="text-center"
-            ></b-form-input>
-            <b-tooltip
-              target="input-experientmethod"
-              triggers="hover"
-              placement="right"
-              variant="info"
-            >{{ methodDescription }}</b-tooltip>
-          </b-form-group>
-        </b-col>
-      </b-row>
-      <b-row>
-        <b-col>
-          <b-form-group
-            description="If unsure of what to upload read the manual."
-            label="Upload experiment"
-            label-for="select-1"
-            class="text-center"
-          >
-            <b-form-file
-              v-model="file"
-              placeholder="Choose a file"
-              drop-placeholder="Drop a file"
-              id="file-1"
-            ></b-form-file>
-          </b-form-group>
-        </b-col>
-      </b-row>
-      <hr />
-      <b-row class="mx-1">
-        <b-col cols="7">
-          <b-form-group
-            description="This is the export format of your equipment."
-            label="Experiment Format"
-            label-for="select-1"
-            class="text-center"
-          >
-            <b-form-select id="select-1" v-model="format" :options="options" size="md"></b-form-select>
-          </b-form-group>
-        </b-col>
-        <b-col cols="5">
-          <b-form-group
-            description="This will help us organize and analyze your data."
-            label="Experiment date"
-            label-for="experiment-date"
-            class="text-center"
-          >
-            <b-form-datepicker
-              id="experiment-date"
-              v-model="date"
-              class="mb-2"
-              placeholder="YYYY-MM-DD"
-              :date-format-options="{ year: 'numeric', month: 'numeric', day: 'numeric' }"
-              locale="en"
-            ></b-form-datepicker>
-          </b-form-group>
-        </b-col>
-      </b-row>
-      <hr />
-      <b-container>
+    <b-overlay :show="loading">
+      <b-modal
+        id="add-experiments-modal"
+        ref="add-experiments-modal"
+        size="md"
+        hide-footer
+        button-size="md"
+        title="Add a new experiment"
+      >
         <b-row>
           <b-col>
-            <b-button @click="uploadExperiment" variant="outline-success" block>Upload Experiment</b-button>
-          </b-col>
-          <b-col>
-            <b-button variant="outline-danger" @click="hideModal" block>Cancel</b-button>
+            <b-alert
+              v-if="importMsg"
+              variant="info"
+              class="text-center"
+              dismissible
+              fade
+            >{{ importMsg }}</b-alert>
           </b-col>
         </b-row>
-      </b-container>
-    </b-modal>
+        <b-row>
+          <b-col>
+            <b-form-group
+              id="fieldset-1"
+              :description="invalidExperimentName"
+              label="Enter your experiment's name"
+              label-for="input-experimentname"
+              class="text-center"
+            >
+              <b-form-input
+                id="input-experimentname"
+                v-model="name"
+                :state="state"
+                trim
+                placeholder="SARS-CoV-2 Run 1"
+                class="text-center"
+              ></b-form-input>
+              <b-tooltip
+                target="input-experimentname"
+                triggers="hover"
+                placement="right"
+                variant="info"
+              >{{ nameDescription }}</b-tooltip>
+            </b-form-group>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col>
+            <b-form-group
+              id="fieldset-1"
+              description="Brief methodology description of your experiment."
+              label="Enter your experiment's methodology"
+              label-for="input-experientmethod"
+              class="text-center"
+            >
+              <b-form-input
+                id="input-experientmethod"
+                v-model="method"
+                :state="state"
+                trim
+                placeholder="extraction-primerprobe-cycling"
+                class="text-center"
+              ></b-form-input>
+              <b-tooltip
+                target="input-experientmethod"
+                triggers="hover"
+                placement="right"
+                variant="info"
+              >{{ methodDescription }}</b-tooltip>
+            </b-form-group>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col>
+            <b-form-group
+              description="If unsure of what to upload read the manual."
+              label="Upload experiment"
+              label-for="select-1"
+              class="text-center"
+            >
+              <b-form-file
+                v-model="file"
+                placeholder="Choose a file"
+                drop-placeholder="Drop a file"
+                id="file-1"
+              ></b-form-file>
+            </b-form-group>
+          </b-col>
+        </b-row>
+        <hr />
+        <b-row class="mx-1">
+          <b-col cols="7">
+            <b-form-group
+              description="This is the export format of your equipment."
+              label="Experiment Format"
+              label-for="select-1"
+              class="text-center"
+            >
+              <b-form-select id="select-1" v-model="format" :options="options" size="md"></b-form-select>
+            </b-form-group>
+          </b-col>
+          <b-col cols="5">
+            <b-form-group
+              description="This will help us organize and analyze your data."
+              label="Experiment date"
+              label-for="experiment-date"
+              class="text-center"
+            >
+              <b-form-datepicker
+                id="experiment-date"
+                v-model="date"
+                class="mb-2"
+                placeholder="YYYY-MM-DD"
+                :date-format-options="{ year: 'numeric', month: 'numeric', day: 'numeric' }"
+                locale="en"
+              ></b-form-datepicker>
+            </b-form-group>
+          </b-col>
+        </b-row>
+        <hr />
+        <b-container>
+          <b-row>
+            <b-col>
+              <b-button @click="uploadExperiment" variant="outline-success" block>Upload Experiment</b-button>
+            </b-col>
+            <b-col>
+              <b-button variant="outline-danger" @click="hideModal" block>Cancel</b-button>
+            </b-col>
+          </b-row>
+        </b-container>
+      </b-modal>
+    </b-overlay>
   </div>
 </template>
 
@@ -167,22 +175,25 @@ export default {
       formData.append("experiment_date", this.date);
       formData.append("experiment_format", this.format);
 
+      // Loading set to true
+      this.loading = true;
+
       // Dispatch uploadExperiment with formData
       await this.$store.dispatch("uploadExperiment", formData).then(() => {
-        // Show alert
-        this.showAlert = true;
+        // Loading set to false
+        this.loading = false;
+
+        // Update data
+        // this.updateData();
       });
 
-      // Update data
-      await this.updateData();
-
       // Set timeout
-      setTimeout(() => {
-        this.hideModal();
-        this.showAlert = false;
-        this.$store.dispatch("clearImportMsg");
-        this.$store.dispatch("loadLastExperiment");
-      }, 1500);
+      // setTimeout(() => {
+      // this.hideModal();
+      // this.showAlert = false;
+      // this.$store.dispatch("clearImportMsg");
+      // this.$store.dispatch("loadLastExperiment");
+      // }, 1500);
     }
   },
 
@@ -213,15 +224,16 @@ export default {
 
   data() {
     return {
-      showAlert: false,
+      loading: false,
       name: "",
       nameDescription:
-        "Should contain enough information to be distinguishable from other experiments",
+        "The title should contain enough information to be distinguishable from other experiments",
       methodDescription:
         "e.g. extraction method, primers and probes used and thermocycler configuration.",
       file: null,
       date: null,
       format: "DA2",
+      method: null,
       options: [
         { value: null, text: "Choose a format" },
         { value: "7500", text: "Applied Biosystems (7500)" },
