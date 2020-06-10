f4e62187be2b88637ddd7efeb828a5fcec3cf23e
VictorCalderon <victorvirgilioc@gmail.com>
2020-06-10 11:44:34 -0400

    query and export functions added

--- backend/qpcr_manager/helpers/__init__.py
+++ backend/qpcr_manager/helpers/__init__.py
@@ -1,7 +1,10 @@
 from qpcr_manager.helpers.preprocessing import (
-    feed_DA2, feed_7500, analyze_experiment, 
-    amped_timeseries, available_markers, export_results, 
-    query_fluorescence
+    feed_DA2, feed_7500, analyze_experiment,
+    amped_timeseries, available_markers, export_results,
+    query_fluorescence, query_samples
 )
 
-__all__ = ['feed_DA2', 'feed_7500', 'analyze_experiment', 'amped_timeseries', 'available_markers', 'query_fluorescence']
+__all__ = [
+    'feed_DA2', 'feed_7500', 'analyze_experiment', 'amped_timeseries',
+    'available_markers', 'query_fluorescence', 'query_samples', 'export_results'
+]
