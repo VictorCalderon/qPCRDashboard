28e350a572b37a579e9c45278cc040879c91048a
VictorCalderon <victorvirgilioc@gmail.com>
2020-06-10 21:06:29 -0400

    function name change

--- backend/qpcr_manager/helpers/__init__.py
+++ backend/qpcr_manager/helpers/__init__.py
@@ -1,10 +1,10 @@
 from qpcr_manager.helpers.preprocessing import (
-    feed_DA2, feed_7500, analyze_experiment,
+    feed_DA2, feed_7500, experiment_statistics,
     amped_timeseries, available_markers, export_results,
     query_fluorescence, query_samples
 )
 
 __all__ = [
-    'feed_DA2', 'feed_7500', 'analyze_experiment', 'amped_timeseries',
+    'feed_DA2', 'feed_7500', 'experiment_statistics', 'amped_timeseries',
     'available_markers', 'query_fluorescence', 'query_samples', 'export_results'
 ]
