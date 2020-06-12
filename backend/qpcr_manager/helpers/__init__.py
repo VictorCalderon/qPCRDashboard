1c09964ac204ccae0870c96f528c62b1b75cfdf2
VictorCalderon <victorvirgilioc@gmail.com>
2020-06-12 11:18:39 -0400

    marker specific getter function dataset

--- backend/qpcr_manager/helpers/__init__.py
+++ backend/qpcr_manager/helpers/__init__.py
@@ -1,10 +1,11 @@
 from qpcr_manager.helpers.preprocessing import (
     feed_DA2, feed_7500, experiment_statistics,
     amped_timeseries, available_markers, export_results,
-    query_fluorescence, query_samples
+    query_fluorescence, query_samples, marker_dataset
 )
 
 __all__ = [
     'feed_DA2', 'feed_7500', 'experiment_statistics', 'amped_timeseries',
-    'available_markers', 'query_fluorescence', 'query_samples', 'export_results'
+    'available_markers', 'query_fluorescence', 'query_samples', 'export_results',
+    'marker_dataset'
 ]
