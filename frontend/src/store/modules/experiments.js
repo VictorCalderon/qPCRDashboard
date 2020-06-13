a87fd040e9736f245c6b74722c807ad42501cf3d
Victor Calderon <victorvirgilioc@gmail.com>
2020-06-13 15:33:01 -0400

    reset state and purge unused code

--- frontend/src/store/modules/experiments.js
+++ frontend/src/store/modules/experiments.js
@@ -1,33 +1,19 @@
 import axios from 'axios'
 import Vue from 'vue'
 
-const state = {
-    addExperimentsModal: false,
-    searchExperimentsModal: false,
-    currentExperimentFile: null,
-    allExperiments: null,
-    importMsg: null,
-    recentExperiments: null,
-    queriedExperiments: [],
-    currentExperiment: null,
-    currentExperimentResults: null
-}
+const state = {}
 
 const mutations = {
+    'INIT_EXPERIMENTS'(state) {
+        Vue.set(state, 'currentExperimentFile', null);
+        Vue.set(state, 'allExperiments', null);
+        Vue.set(state, 'queriedExperiments', []);
+        Vue.set(state, 'currentExperiment', null);
+        Vue.set(state, 'currentExperimentResults', null);
+    },
+
     'LOAD_ALL_EXPERIMENTS'(state, experiments) {
         Vue.set(state, 'allExperiments', experiments)
-    },
-
-    'TOGGLE_ADD_EXPERIMENTS'(state) {
-        Vue.set(state, 'addExperimentsModal', !state.addExperimentsModal)
-    },
-
-    'TOGGLE_SEARCH_EXPERIMENTS'(state) {
-        Vue.set(state, 'searchExperimentsModal', !state.searchExperimentsModal)
-    },
-
-    'POST_EXPERIMENT_MSG'(state, errorMsg) {
-        Vue.set(state, 'postExperimentMsg', errorMsg)
     },
 
     'LOAD_NEXT_EXPERIMENTS'(state, nextUrl) {
@@ -42,10 +28,6 @@ const mutations = {
         Vue.set(state, 'currentExperiment', experiment)
     },
 
-    'IMPORT_MSG'(state, msg) {
-        Vue.set(state, 'importMsg', msg)
-    },
-
     'UPDATE_MSG'(state, msg) {
         Vue.set(state, 'updateMsg', msg)
     },
@@ -56,40 +38,6 @@ const mutations = {
 
     'CLEAR_EXPERIMENTS'(state) {
         Vue.set(state, 'allExperiments', [])
-    },
-
-    'LOAD_RECENT_EXPERIMENTS'(state) {
-        if (state.recentExperiments == null && state.allExperiments != null) {
-            const recentExperiments = state.allExperiments.slice(0, 6);
-            Vue.set(state, 'recentExperiments', recentExperiments)
-        }
-        else return void 0
-
-    },
-
-    'ADD_RECENT_EXPERIMENT'(state, experiment) {
-        // Grab current data
-        let recentExperiments = state.recentExperiments
-
-        // Push new experiment
-        recentExperiments.push(experiment)
-
-        // Set as new data
-        Vue.set(state, 'recentExperiments', recentExperiments)
-    },
-
-    'REMOVE_RECENT_EXPERIMENT'(state, experiment) {
-        // Grab current data
-        let recentExperiments = state.recentExperiments
-
-        // Find index
-        let index = recentExperiments.findIndex(x => x.id === experiment.id);
-
-        // Push new experiment
-        recentExperiments.splice(index, 1)
-
-        // Set as new data
-        Vue.set(state, 'recentExperiments', recentExperiments)
     },
 
     'EXPERIMENTS_QUERY'(state, experiments) {
@@ -110,6 +58,10 @@ const mutations = {
 }
 
 const actions = {
+    initExperiments({ commit }) {
+        commit('INIT_EXPERIMENTS')
+    },
+
     loadExperiments({ commit }) {
         axios.get('api/v1/experiments', {
         }).then(res => {
@@ -118,9 +70,7 @@ const actions = {
                 commit('LOAD_NEXT_EXPERIMENTS', res.data.next);
                 commit('LOAD_PREV_EXPERIMENTS', res.data.prev);
             }
-        }).then(
-            commit('LOAD_RECENT_EXPERIMENTS')
-        )
+        })
     },
 
     async getMarkers({ commit }) {
@@ -135,40 +85,8 @@ const actions = {
         })
     },
 
-    toggleAddExperiments({ commit }) {
-        commit('TOGGLE_ADD_EXPERIMENTS')
-    },
-
-    toggleSearchExperiments({ commit }) {
-        commit('TOGGLE_SEARCH_EXPERIMENTS')
-    },
-
-    selectExperiment({ getters, commit }, experiment) {
+    selectExperiment({ commit }, experiment) {
         commit('SELECT_EXPERIMENT', experiment);
-
-        // Check if experiment is already added
-        if (getters.recentExperiments) {
-
-            // Search experiment in recent if we have it
-            const search = getters.recentExperiments.filter(p => p.id == experiment.id)
-
-            if (search.length == 0) {
-                commit('ADD_RECENT_EXPERIMENT', experiment);
-            }
-        }
-
-    },
-
-    loadSingleExperiment({ state, commit }, experiment) {
-        // Grab current data
-        let experiments = state.allExperiments
-
-        // Push new experiment
-        experiments.push(experiment)
-
-        // Make this the new experiment
-        commit('SET_AS_RECENT_EXPERIMENTS', experiments)
-
     },
 
     updateCurrentExperiment({ commit, getters }) {
@@ -176,12 +94,6 @@ const actions = {
             commit('SELECT_EXPERIMENT', res.data.experiment)
         })
     },
-
-    // selectCurrentExperiment({ commit, getters }) {
-    //     if (getters.currentExperiment) {
-    //         commit('SELECT_EXPERIMENT', getters.currentExperiment.id)
-    //     }
-    // },
 
     loadLastExperiment({ commit }) {
         axios.get('api/v1/experiments/lastexperiment', {
@@ -196,13 +108,6 @@ const actions = {
                 commit('EXPORT_EXPERIMENT_FILE', res.data.file)
             })
         }
-    },
-
-    deleteExperiment({ commit }, experiment_id) {
-        axios.delete(`api/v1/experiments/${experiment_id}`, {
-        }).then(res => {
-            commit('EXPORT_MSG', res.data.msg)
-        })
     },
 
     clearExperiments({ commit }) {
@@ -242,31 +147,11 @@ const actions = {
     clearUpdateMsg({ commit }) {
         commit('UPDATE_MSG', null)
     },
-
-    clearImportMsg({ commit }) {
-        commit('IMPORT_MSG', null)
-    },
 }
 
 const getters = {
     allExperiments(state) {
         return state.allExperiments ? state.allExperiments : null
-    },
-
-    recentExperiments(state) {
-        return state.recentExperiments ? state.recentExperiments.reverse().slice(0, 6) : null;
-    },
-
-    addExperimentsModal(state) {
-        return state.addExperimentsModal
-    },
-
-    searchExperimentsModal(state) {
-        return state.searchExperimentsModal
-    },
-
-    postExperimentError(state) {
-        return state.postExperimentMsg
     },
 
     currentExperiment(state) {
