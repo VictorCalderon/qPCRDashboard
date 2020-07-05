7842908352430e8b14b0464e8a1f80b202e91745
VictorCalderon <victorvirgilioc@gmail.com>
2020-07-04 20:33:11 -0400

    Adjustments view

--- frontend/src/views/Adjustments.vue
+++ frontend/src/views/Adjustments.vue
@@ -1,13 +1,13 @@
 <template>
   <b-container flex="md">
-    <b-row class="justify-content-center mt-3">
+    <b-form-row class="justify-content-center mt-3">
       <b-col sm="12" md="10" lg="6" class="text-center">
         <LocationSchema></LocationSchema>
       </b-col>
       <b-col sm="12" md="10" lg="6" class="text-center">
         <UserSettings></UserSettings>
       </b-col>
-    </b-row>
+    </b-form-row>
   </b-container>
 </template>
 
