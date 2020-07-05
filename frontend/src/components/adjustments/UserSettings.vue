a0ea3f059d87a6d6fbe7dc3708af41adcc3b5276
VictorCalderon <victorvirgilioc@gmail.com>
2020-07-04 20:29:43 -0400

    dashboard settings

--- frontend/src/components/adjustments/UserSettings.vue
+++ frontend/src/components/adjustments/UserSettings.vue
@@ -6,7 +6,21 @@
     header-bg-variant="dark"
     header-text-variant="white"
     class="m-2"
-  ></b-card>
+  >
+    <b-form-row class="justify-content-end">
+      <b-col cols="3">
+        <h5 class="my-2">Dashboard Map Center</h5>
+      </b-col>
+      <b-col cols="3">
+        <label for="center-langitude">Latitude</label>
+        <b-form-input id="center-langitude" type="number"></b-form-input>
+      </b-col>
+      <b-col cols="3">
+        <label for="center-langitude">Longitude</label>
+        <b-form-input id="center-longitude" type="number"></b-form-input>
+      </b-col>
+    </b-form-row>
+  </b-card>
 </template>
 
 <script>
