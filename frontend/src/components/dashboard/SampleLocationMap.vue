fc61570cca85a1533e3c7a9af6873c43e0210e2e
VictorCalderon <victorvirgilioc@gmail.com>
2020-07-04 20:34:02 -0400

    Leaflet map implementation

--- frontend/src/components/dashboard/SampleLocationMap.vue
+++ frontend/src/components/dashboard/SampleLocationMap.vue
@@ -1,28 +1,91 @@
 <template>
-  <div>
-    <l-map :zoom="zoom" :center="center">
-      <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
-      <l-marker :lat-lng="marker"></l-marker>
+  <div style="height: 405px; width: 100%">
+    <l-map
+      v-if="showMap"
+      :zoom="zoom"
+      :center="center"
+      :options="mapOptions"
+      style="height: 100%"
+      @update:center="centerUpdate"
+      @update:zoom="zoomUpdate"
+    >
+      <l-tile-layer :url="url" :attribution="attribution" />
+      <div v-for="(s, i) in samplingSites" :key="i">
+        <l-circle
+          :lat-lng="s.loc"
+          :radius="s.totalSamples"
+          :color="siteColor[i]"
+          :fillColor="siteColor[i]"
+          :opacity="0.8"
+        >
+          <l-tooltip :options="{ permanent: false, interactive: true }">
+            <div @click="innerClick(s.description)">{{s.name}}</div>
+          </l-tooltip>
+        </l-circle>
+      </div>
     </l-map>
   </div>
 </template>
 
 <script>
-import { LMap, LTileLayer, LMarker, latLng } from "vue2-leaflet";
+import { latLng } from "leaflet";
+import { LMap, LTileLayer, LCircle, LTooltip } from "vue2-leaflet";
+
 export default {
-  components: { LMap, LTileLayer, LMarker },
+  components: {
+    LMap,
+    LTileLayer,
+    LCircle,
+    LTooltip
+  },
   data() {
     return {
-      zoom: 13,
-      center: latLng(47.41322, -1.219482),
-      url: "http://{s}.tile.osm.org/{z}/{x}/{y}.png",
+      zoom: 8,
+      center: latLng(19.007237, -70.41502),
+      url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
       attribution:
         '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-      marker: latLng(47.41322, -1.219482)
+      currentZoom: 11.5,
+      currentCenter: latLng(19.007237, -70.41502),
+      showParagraph: false,
+      mapOptions: {
+        zoomSnap: 0.5
+      },
+      showMap: true,
+      siteColor: ["#DD614A", "#F48668", "#5AB1BB", "#A5C882", "#F7DD72"]
     };
+  },
+  computed: {
+    samplingSites() {
+      return [
+        {
+          loc: latLng(18.4358, -69.9853),
+          name: "Sede Central",
+          totalSamples: 19800
+        },
+        {
+          loc: latLng(19.4548, -70.6929),
+          name: "Santiago",
+          totalSamples: 12800
+        },
+        {
+          loc: latLng(19.22064, -70.5632),
+          name: "La Vega",
+          totalSamples: 11000
+        }
+      ];
+    }
+  },
+  methods: {
+    zoomUpdate(zoom) {
+      this.currentZoom = zoom;
+    },
+    centerUpdate(center) {
+      this.currentCenter = center;
+    },
+    innerClick(description) {
+      alert(description);
+    }
   }
 };
 </script>
-
-<style>
-</style>
