5bf0c3db1d7dcea8b57b9dbbcdcab8e4071f34ed
VictorCalderon <victorvirgilioc@gmail.com>
2020-07-09 17:06:02 -0400

    Map helper functions added

--- backend/qpcr_manager/helpers/__init__.py
+++ backend/qpcr_manager/helpers/__init__.py
@@ -2,11 +2,11 @@ from qpcr_manager.helpers.preprocessing import (
     feed_DA2, feed_7500, experiment_statistics,
     amped_timeseries, available_markers, export_results,
     query_fluorescence, query_samples, marker_dataset,
-    get_brief, amp_stat_data, tag_distrib, location_moficiations
+    get_brief, amp_stat_data, tag_distrib, location_moficiations, get_located_samples
 )
 
 __all__ = [
     'feed_DA2', 'feed_7500', 'experiment_statistics', 'amped_timeseries',
     'available_markers', 'query_fluorescence', 'query_samples', 'export_results',
-    'marker_dataset', 'get_brief', 'amp_stat_data', 'tag_distrib', 'location_moficiations'
+    'marker_dataset', 'get_brief', 'amp_stat_data', 'tag_distrib', 'location_moficiations', 'get_located_samples'
 ]
