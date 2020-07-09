aab230c6fa5ceb3a0ba9a1c67663134ea3abb1ab
VictorCalderon <victorvirgilioc@gmail.com>
2020-07-09 18:15:41 -0400

    Sample counts

--- frontend/src/components/dashboard/LocatedSamplesCount.vue
+++ frontend/src/components/dashboard/LocatedSamplesCount.vue
@@ -1,58 +1,70 @@
 <template>
-  <PieChart :chartData="chartData" :options="chartConfig" :height="320"></PieChart>
+  <BarChart :chartData="chartData" :options="chartConfig" :height="250" v-if="smallDataset"></BarChart>
+  <HBarChart :chartData="chartData" :options="chartConfig" :height="250" v-else></HBarChart>
 </template>
 
 <script>
-import PieChart from "@/components/charts/PieChart.js";
+import BarChart from "@/components/charts/BarChart.js";
+import HBarChart from "@/components/charts/HBarChart.js";
 
 export default {
   components: {
-    PieChart
+    BarChart,
+    HBarChart
   },
+
+  computed: {
+    smallDataset() {
+      if (this.$store.getters.samplingSites) {
+        if (this.$store.getters.samplingSites.length > 1) {
+          return false;
+        } else return true;
+      } else return null;
+    },
+
+    samplingLabels() {
+      return this.$store.getters.samplingSites.map(p => {
+        return p.name;
+      });
+    },
+
+    samplingDatasets() {
+      return this.$store.getters.samplingSites.map(p => {
+        return p.count;
+      });
+    },
+
+    samplingColors() {
+      return this.$store.getters.samplingSites.map(p => {
+        return p.bgColor;
+      });
+    },
+
+    samplingSites() {
+      return this.$store.getters.samplingSites;
+    },
+
+    chartData() {
+      if (this.samplingSites) {
+        return {
+          labels: this.samplingLabels,
+          datasets: [
+            {
+              label: "Sampling Site Counts",
+              data: this.samplingDatasets,
+              backgroundColor: this.samplingColors,
+              hoverWidth: 2,
+              hoverColor: "#000"
+            }
+          ]
+        };
+      } else return {};
+    }
+  },
+
   data() {
     return {
-      chartData: {
-        labels: [
-          "Sede Central",
-          "Naco",
-          "Santiago",
-          "La Vega",
-          "Punta Cana",
-          "Samaná",
-          "San Juan",
-          "Monte Cristi"
-        ],
-        datasets: [
-          {
-            label: "Samples analyzed",
-            data: [3212, 1921, 1232, 989, 802, 702, 501, 339, 121, 0],
-            backgroundColor: [
-              "#DD614A",
-              "#F48668",
-              "#5AB1BB",
-              "#A5C882",
-              "#F7DD72",
-              "#DD614A",
-              "#F48668",
-              "#5AB1BB",
-              "#A5C882",
-              "#F7DD72"
-            ],
-            hoverWidth: 2,
-            hoverColor: "#000"
-          }
-        ]
-      },
       chartConfig: {
-        // scales: {
-        //   xAxes: [
-        //     {
-        //       ticks: {
-        //         fontSize: 12
-        //       }
-        //     }
-        //   ]
-        // },
         maintainAspectRatio: true,
         reponsive: true,
         legend: {
