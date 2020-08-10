0f84a2d40dd9db621690f41084f434edcaeea139
VictorCalderon <victorvirgilioc@gmail.com>
2020-08-10 18:52:19 -0400

    new score column

--- backend/qpcr_manager/models/results.py
+++ backend/qpcr_manager/models/results.py
@@ -10,6 +10,7 @@ class Result(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     amp_status = db.Column(db.Boolean)
     amp_cq = db.Column(db.Float)
+    score = db.Column(db.Float)
 
     sample_id = db.Column(db.Integer, db.ForeignKey('samples.id'))
     marker_id = db.Column(db.Integer, db.ForeignKey('markers.id'))
