8a110b1c45a7d0e6987d736baa5e185a4e64734e
VictorCalderon <victorvirgilioc@gmail.com>
2020-08-19 18:00:04 -0400

    Brand new experiment View

---
+++ frontend/src/views/Experiments.vue
@@ -0,0 +1,100 @@
+<template>
+  <div>
+    <b-form-row class="mt-2" v-if="currentExperiment">
+      <b-col lg="8" md="12" sm="12" xs="12">
+        <b-card
+          no-body
+          class="card-height mb-2"
+          header-bg-variant="dark"
+          header-text-variant="white"
+        >
+          <b-tabs card pills justified>
+            <b-tab title="Amplification Plot" lazy>
+              <AmpPlot></AmpPlot>
+            </b-tab>
+            <b-tab title="PCA - KMeans" lazy>
+              <PCAKMeans></PCAKMeans>
+            </b-tab>
+            <b-tab title="Maximum Gradient" lazy>
+              <MaxGrad></MaxGrad>
+            </b-tab>
+          </b-tabs>
+        </b-card>
+      </b-col>
+      <b-col lg="4" md="12" sm="12" xs="12">
+        <SampleTable></SampleTable>
+      </b-col>
+    </b-form-row>
+
+    <b-form-row class="mt-5 justify-content-center" v-show="!currentExperiment">
+      <b-col cols="8">
+        <b-card class="text-center rounded">
+          <div
+            class="rounded py-2"
+          >You don't seem to be working on any experiments right now. Use the engine to search for experiments.</div>
+          <b-row align-h="center" class="mt-3">
+            <b-button variant="info" v-b-modal.search-experiments-modal>Search Experiment</b-button>
+          </b-row>
+        </b-card>
+      </b-col>
+    </b-form-row>
+  </div>
+</template>
+
+<script>
+import AmpPlot from "@/components/experiments/AmpPlot";
+import SampleTable from "@/components/experiments/SampleTable";
+import PCAKMeans from "@/components/experiments/PCAKMeans";
+import MaxGrad from "@/components/experiments/MaxGrad";
+
+export default {
+  components: {
+    AmpPlot,
+    PCAKMeans,
+    SampleTable,
+    MaxGrad
+  },
+
+  methods: {
+    loadExperiments() {
+      this.$store.dispatch("loadExperiments");
+    }
+  },
+
+  computed: {
+    currentExperiment() {
+      return this.$store.getters.currentExperiment;
+    },
+    
+    allExperiments() {
+      return this.$store.getters.allExperiments;
+    },
+
+    loadingAmpPlot() {
+      return this.$store.getters.loadingAmpPlot
+    }
+  },
+
+  created() {
+    this.loadExperiments();
+  },
+
+  data() {
+    return {
+      loading: false
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.card-height {
+  height: 85vh;
+}
+
+@media (max-width: 480px) {
+  .card-height {
+    height: 65vh;
+  }
+}
+</style>
