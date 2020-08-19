658b758c5401bde219ab5787c2f3807523ad44be
VictorCalderon <victorvirgilioc@gmail.com>
2020-08-19 18:01:28 -0400

    Analysis helper functions added

--- backend/qpcr_manager/helpers/__init__.py
+++ backend/qpcr_manager/helpers/__init__.py
@@ -1,14 +1,13 @@
 from qpcr_manager.helpers.preprocessing import (
     feed_DA2, feed_7500, experiment_statistics,
-    amped_timeseries, available_markers, export_results,
-    query_fluorescence, query_samples, marker_dataset,
+    available_markers, query_samples, marker_dataset, experiment_pca_kmeans,
     get_brief, amp_stat_data, tag_distrib, location_moficiations,
-    get_located_samples, sample_table, experiment_fluorescences
+    get_located_samples, sample_table, experiment_fluorescences, maximum_gradient
 )
 
 __all__ = [
-    'feed_DA2', 'feed_7500', 'experiment_statistics', 'amped_timeseries',
-    'available_markers', 'query_fluorescence', 'query_samples', 'export_results',
+    'feed_DA2', 'feed_7500', 'experiment_statistics',
+    'available_markers', 'query_samples', 'experiment_pca_kmeans',
     'marker_dataset', 'get_brief', 'amp_stat_data', 'tag_distrib', 'location_moficiations', 'get_located_samples',
-    'sample_table', 'experiment_fluorescences'
+    'sample_table', 'experiment_fluorescences', 'maximum_gradient'
 ]
