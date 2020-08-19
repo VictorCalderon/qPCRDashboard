e4a2c128cf2f69c2a84cc0be414d8c8c13dd2bdc
VictorCalderon <victorvirgilioc@gmail.com>
2020-08-19 18:00:37 -0400

    Dashboard adjustments added

--- frontend/src/views/Adjustments.vue
+++ frontend/src/views/Adjustments.vue
@@ -1,24 +1,41 @@
 <template>
-  <b-container flex="md">
-    <b-form-row class="justify-content-center mt-3">
-      <b-col sm="12" md="10" lg="7" class="text-center">
-        <LocationSchema></LocationSchema>
-      </b-col>
-      <b-col sm="12" md="10" lg="5" class="text-center">
-        <MapSettings></MapSettings>
-      </b-col>
-    </b-form-row>
-  </b-container>
+  <b-form-row class="justify-content-center mt-1">
+    <b-col sm="12" md="10" lg="6">
+      <b-form-row>
+        <b-col cols="12">
+          <LocationSchema></LocationSchema>
+        </b-col>
+        <b-col cols="12"></b-col>
+      </b-form-row>
+    </b-col>
+    <b-col sm="12" md="10" lg="4">
+      <b-form-row>
+        <b-col cols="12">
+          <MapSettings></MapSettings>
+        </b-col>
+        <b-col cols="12">
+          <TargetGroups></TargetGroups>
+        </b-col>
+        <b-col cols="12">
+          <MarkerTarget></MarkerTarget>
+        </b-col>
+      </b-form-row>
+    </b-col>
+  </b-form-row>
 </template>
 
 <script>
 import LocationSchema from "@/components/adjustments/LocationSchema.vue";
 import MapSettings from "@/components/adjustments/MapSettings.vue";
+import TargetGroups from "@/components/adjustments/TargetGroups.vue";
+import MarkerTarget from "@/components/adjustments/MarkerTarget.vue";
 
 export default {
   components: {
     LocationSchema,
-    MapSettings
+    MapSettings,
+    TargetGroups,
+    MarkerTarget
   }
 };
 </script>
