ce32fc1816a9615b945c370952301f9d3e39f72e
VictorCalderon <victorvirgilioc@gmail.com>
2020-08-19 19:42:27 -0400

    Removed unnecesary helper functions

--- backend/qpcr_manager/helpers/__init__.py
+++ backend/qpcr_manager/helpers/__init__.py
@@ -1,13 +1,11 @@
 from qpcr_manager.helpers.preprocessing import (
-    feed_DA2, feed_7500, experiment_statistics,
-    available_markers, query_samples, marker_dataset, experiment_pca_kmeans,
+    feed_DA2, feed_7500, available_markers, query_samples, marker_dataset, experiment_pca_kmeans,
     get_brief, amp_stat_data, tag_distrib, location_moficiations,
     get_located_samples, sample_table, experiment_fluorescences, maximum_gradient
 )
 
 __all__ = [
-    'feed_DA2', 'feed_7500', 'experiment_statistics',
-    'available_markers', 'query_samples', 'experiment_pca_kmeans',
+    'feed_DA2', 'feed_7500', 'available_markers', 'query_samples', 'experiment_pca_kmeans',
     'marker_dataset', 'get_brief', 'amp_stat_data', 'tag_distrib', 'location_moficiations', 'get_located_samples',
     'sample_table', 'experiment_fluorescences', 'maximum_gradient'
 ]
