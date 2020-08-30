07d84976e50be6dd2197774e54551373652582d8
VictorCalderon <victorvirgilioc@gmail.com>
2020-08-30 01:24:46 -0400

    Load q2000 parser function

--- backend/qpcr_manager/helpers/__init__.py
+++ backend/qpcr_manager/helpers/__init__.py
@@ -1,11 +1,15 @@
 from qpcr_manager.helpers.preprocessing import (
-    feed_DA2, feed_7500, available_markers, query_samples, experiment_pca_kmeans,
+    available_markers, query_samples, experiment_pca_kmeans,
     get_brief, amp_stat_data, tag_distrib, location_moficiations,
-    get_located_samples, sample_table, experiment_fluorescences, maximum_gradient, load_DA2
+    get_located_samples, sample_table, experiment_fluorescences, maximum_gradient,
+    load_DA2, load_7500, load_q2000
 )
 
+from zipfile import BadZipFile
+
 __all__ = [
-    'feed_DA2', 'feed_7500', 'available_markers', 'query_samples', 'experiment_pca_kmeans',
+    'available_markers', 'query_samples', 'experiment_pca_kmeans',
     'get_brief', 'amp_stat_data', 'tag_distrib', 'location_moficiations', 'get_located_samples',
-    'sample_table', 'experiment_fluorescences', 'maximum_gradient', 'load_DA2'
+    'sample_table', 'experiment_fluorescences', 'maximum_gradient', 'load_DA2', 'load_7500', 'load_q2000',
+    'BadZipFile'
 ]
