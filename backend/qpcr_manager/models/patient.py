098b440612b831837e7138f7ff4b58f496d48b3c
Victor Calderon <44529243+VictorCalderon@users.noreply.github.com>
2020-09-03 10:08:58 -0400

    patient schema update

--- backend/qpcr_manager/models/patient.py
+++ backend/qpcr_manager/models/patient.py
@@ -10,9 +10,15 @@ class Patient(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     full_name = db.Column(db.String, nullable=False)
     cedula = db.Column(db.String, nullable=False)
+    barcode = db.Column(db.String, nullable=False)
     birth_date = db.Column(db.Date, nullable=False)
+    priority = db.Column(db.Integer, nullable=False)
     suspected_illness = db.Column(db.String)
     is_male = db.Column(db.Boolean, nullable=False)
+    step = db.Column(db.Integer, nullable=False)
+
+    location_id = db.Column(db.Integer, db.ForeignKey('locations.id'))
+    location = db.relationship('Location', backref="patients", lazy=True)
 
 
     def __repr__(self):
