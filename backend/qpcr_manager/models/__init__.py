44c4d06dd016229481304f15180b352b5a63481a
Victor Calderon <44529243+VictorCalderon@users.noreply.github.com>
2020-09-03 03:51:03 -0400

    New Patient Model

--- backend/qpcr_manager/models/__init__.py
+++ backend/qpcr_manager/models/__init__.py
@@ -3,6 +3,7 @@ from qpcr_manager.models.blacklist import TokenBlacklist
 from qpcr_manager.models.marker import Marker
 from qpcr_manager.models.experiment import Experiment
 from qpcr_manager.models.sample import Sample
+from qpcr_manager.models.patient import Patient
 from qpcr_manager.models.fluorescence import Fluorescence
 from qpcr_manager.models.results import Result
 from qpcr_manager.models.setting import Setting
@@ -12,5 +13,6 @@ from qpcr_manager.models.target import Target
 
 __all__ = [
     "User", "TokenBlacklist", "Marker", "Experiment", "Sample",
-    "Fluorescence", "Setting", "Result", "Location", "Target"
+    "Fluorescence", "Setting", "Result", "Location", "Target",
+    "Patient"
 ]
