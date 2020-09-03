7759198299f7776f4ebeda4b8742fa0965aee822
Victor Calderon <44529243+VictorCalderon@users.noreply.github.com>
2020-09-02 22:19:34 -0400

    exported options

--- backend/qpcr_manager/helpers/__init__.py
+++ backend/qpcr_manager/helpers/__init__.py
@@ -1,6 +1,6 @@
 from qpcr_manager.helpers.preprocessing import (
     available_markers, query_samples, experiment_pca_kmeans,
-    get_brief, amp_stat_data, tag_distrib, location_moficiations,
+    get_brief, amp_stat_data, tag_distrib, location_modifications,
     get_located_samples, sample_table, experiment_fluorescences, maximum_gradient,
     load_DA2, load_7500, load_q2000
 )
@@ -9,7 +9,7 @@ from zipfile import BadZipFile
 
 __all__ = [
     'available_markers', 'query_samples', 'experiment_pca_kmeans',
-    'get_brief', 'amp_stat_data', 'tag_distrib', 'location_moficiations', 'get_located_samples',
+    'get_brief', 'amp_stat_data', 'tag_distrib', 'location_modifications', 'get_located_samples',
     'sample_table', 'experiment_fluorescences', 'maximum_gradient', 'load_DA2', 'load_7500', 'load_q2000',
     'BadZipFile'
 ]
