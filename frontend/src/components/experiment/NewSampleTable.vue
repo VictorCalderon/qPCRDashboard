d66efc45853a4492216a5f06b71d44dff2c72bf0
Victor Calderon <44529243+VictorCalderon@users.noreply.github.com>
2020-09-03 05:52:51 -0400

    New Sample table

---
+++ frontend/src/components/experiment/NewSampleTable.vue
@@ -0,0 +1,90 @@
+<template>
+  <div>
+    <b-table
+      striped
+      class="text-center"
+      scrollable
+      hover
+      :items="currentExperimentPlate"
+      :fields="fields"
+    >
+    
+      <template v-slot:cell(sex)="data">
+        <span>{{ sexOptions.find(s => s.value == data.item.sex)['text'] }}</span>
+      </template>
+
+      <template v-slot:cell(priority)="data">
+        <span
+          :style="{color: data.item.priority == 0 ? '#36382E' : '#296EB4'}"
+        >{{ priorityOptions.find(p => p.value == data.item.priority)['text'] }}</span>
+      </template>
+
+      <template v-slot:cell(geoZone)="data" v-if="availableGeoZones">
+        <span>{{ availableGeoZones.find(g => g.value == data.item.geoZone)['text'] }}</span>
+      </template>
+
+      <template v-slot:cell(step)="data" v-if="stepOptions">
+        <span
+         :style="{color: data.item.step == 99 ? '#E94F37' : '#393E41'}"
+        >{{ stepOptions.find(s => s.value == data.item.step)['text'] }}</span>
+      </template>
+    </b-table>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      fields: [
+        { key: 'well'},
+        { key: "name" },
+        { key: "barcode" },
+        { key: "cedula" },
+        { key: "sex" },
+        { key: "age" },
+        { key: "priority" },
+        // { key: "description" },
+        { key: "geoZone" },
+        { key: "step" },
+      ],
+    };
+  },
+
+  computed: {
+    currentExperimentPlate() {
+      return this.$store.getters.newSamplePlate;
+    },
+  
+    sampleLocationSchemas() {
+      return this.$store.getters.sampleLocationSchemas;
+    },
+
+    stepOptions() {
+      return this.$store.getters.stepOptions
+    },
+
+    sexOptions() {
+      return this.$store.getters.sexOptions
+    },
+
+    priorityOptions() {
+      return this.$store.getters.priorityOptions
+    },
+
+    availableGeoZones() {
+      if (this.sampleLocationSchemas) {
+        return [
+          ...this.sampleLocationSchemas.map((loc) => {
+            return {
+              text: loc.location,
+              value: loc.id,
+            };
+          }),
+        ];
+      } else return [{ text: "No Schema available", value: null }];
+    },
+  },
+};
+</script>
